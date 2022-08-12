d6e8ebfc8c1602fd90a31624f2dd5410eafbf846
Reza-Keymasi <rezakeymasi1996@gmail.com>
2022-08-12 18:52:35 +0430

    Provider wraped to <App /> and store passed in as a props. now it is useable in all components that will define in App

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
 import { BrowserRouter } from "react-router-dom";
+
+import store from "./app/store.js"
+import {Provider} from "react-redux"
 import "antd/dist/antd.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
 
