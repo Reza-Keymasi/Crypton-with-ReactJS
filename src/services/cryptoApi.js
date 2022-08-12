8a8245d5240cceb06277c55d0ab8c68b4d27bf8d
Reza-Keymasi <rezakeymasi1996@gmail.com>
2022-08-12 18:45:28 +0430

    crypto.Api.js is a file that is create to get data from api by using redux toolkit. Also i defined a hook to use this data in all component i need

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,27 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '864f0e6612msh75ea59dd14b345ap1fddf0jsn9c358f026273',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+// const BASE_URL = "https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd/exchanges";
+
+
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath : "cryptoApi",
+    baseQuery : fetchBaseQuery({ baseUrl }),
+    endpoints : (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`)
+        }),
+    })
+})
+
+
+export const { useGetCryptosQuery } = cryptoApi;
