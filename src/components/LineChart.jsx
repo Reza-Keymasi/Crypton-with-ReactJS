66d5246fbd9b64def48cff6781cee7e5807fa3a4
Reza-Keymasi <rezakeymasi1996@gmail.com>
2022-08-16 15:27:00 +0430

    LineChart.jsx is just a component to show the price on the chart and it will be add to CryptoDetails.jsx

---
+++ src/components/LineChart.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Line, Chart } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js'
+
+import { Col, Row, Typography } from 'antd';
+
+// const { Title } = Typography;
+
+const LineChart = ({ coinHistory, currentPrice, coinName, coinChange }) => {
+  const coinPrice = [];
+  const coinTimestamp = [];
+
+  for (let i = 0; i < coinHistory?.length; i += 1) {
+    coinPrice.push(coinHistory[i]?.price);
+  }
+
+  for (let i = 0; i < coinHistory?.length; i += 1) {
+    coinTimestamp.push(new Date(coinHistory[i]?.timestamp).toLocaleDateString());
+  }
+  console.log(coinPrice);
+
+
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: 'Price In USD',
+        data: coinPrice,
+        fill: false,
+        backgroundColor: '#0071bd',
+        borderColor: '#0071bd',
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  )
+  
+  return (
+    <>
+      <Row className="chart-header">
+        <Typography.Title level={2} className="chart-title">{coinName} Price Chart </Typography.Title>
+        <Col className="price-container">
+          <Typography.Title level={5} className="price-change">Change: {coinChange}%</Typography.Title>
+          <Typography.Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Typography.Title>
+        </Col>
+      </Row>
+      <Line data={data} options={options} />
+    </>
+  );
+};
+
+export default LineChart;
